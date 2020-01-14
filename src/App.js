821a8a087083e74d2db72ddaca6429fa03c25412
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-01-14 13:38:46 +0100

    AMF frontend Left Panel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './output.css';
+import { LeftPanel } from './components/LeftPanel';
+import {Field, Form, Formik} from 'formik';
+import styled from 'styled-components';
+import * as Yup from 'yup';
+
+const Err = styled.div (
+  {
+    color: 'pavioletred',
+    align:'center'
+  }
+);
+
+const UploadSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too Short! Name cannot be less than 3 characters')
+    .max(40, 'Too Long! Name cannot be longer than 3 characters')
+    .required('Required'),
+  email: Yup.string()
+    .required('Required, a file needs to be attached!'),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App h-screen flex">
+      <LeftPanel/>
+      <div className='w-3/5 bg-gray-200 text-center'>
+        <Formik
+          initialValues = {{
+            name: '',
+            file: ''
+          }} 
+          validationSchema = {UploadSchema}
+          onSubmit = {
+              values => (
+                console.log(values)
+            )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {({errors, touched, setFieldValue}) => (
+            <Form className='Parse-Form'>
+              <div>
+                <Field name='name' placeholder='Let me know your name'/>
+                {errors.name && touched.name? <Err>{errors.name}</Err>: null}
+              </div>
+              <Field name='amf' type='file' onChange = {event => {
+                setFieldValue('file', event.currentTarget.files[0])
+              }} />
+              {errors.file && touched.file? <Err>{errors.file}</Err>: null}
+              <button className='p-4 bg-orange-300'>Parse AMF</button>
+            </Form>
+          )}
+        </Formik>
+
+
+      </div>
     </div>
   );
 }
---
+++ src/components/LeftPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Introducer from '../utils/introducer.svg';
+
+export const LeftPanel  = ()=> (
+    <div className='w-2/5 bg-orange-300'>
+      <div className='p-10 Logo text-2xl'>Parser.io</div>
+      <div className='mx-auto my-24 flex justify-center'>
+        <img src={Introducer} alt='Explainer Standing with Direction for app use'/>
+      </div>
+      <div className='justify-center flex'>
+          <div className='item-1 text-3xl Intro-Text'>In One Click.</div>
+      </div>
+      <div className='small text-center m-5 Intro-Sub-Text'>Attach your AMF and get it parsed for you.</div>
+    </div>
+)
