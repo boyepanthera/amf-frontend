541f215d0d54858a64d3e82e6371d20ecc8f2fa1
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-13 09:53:46 +0100

    rewrite downloadFile func

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Field, Form, Formik} from 'formik';
 import styled from 'styled-components';
 import * as Yup from 'yup';
 import axios from 'axios';
+import download from 'downloadjs';
 
 const Err = styled.div (
   {
@@ -27,7 +28,7 @@ const UploadSchema = Yup.object().shape({
 
 function App() {
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  const [err, setErr] = useState(null);
+  // const [err, setErr] = useState(null);
   let [submitted, setSubmit] = useState(false);
   const [response, setResponse] = useState(false);
   const files = acceptedFiles.map(file => (
@@ -51,21 +52,26 @@ function App() {
     })
   }
 
-  const downloadFile = (event) =>{
-    // console.log (event.target);
-    // console.dir(event.target.innerText);
-    const filename = event.target.innerText;
-    axios.get(`http://localhost:5003/files/${filename}`)
-    .then(blob=> {
-      const url = window.URL.createObjectURL(new Blob([blob]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', filename);
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-    })
-    .catch(err=> setErr(err)); 
+  // const downloadFile = (event) =>{
+  //   const filename = event.target.innerText;
+  //   axios.get(`http://localhost:5003/files/${filename}`)
+  //   .then(blob=> {
+  //     const url = window.URL.createObjectURL(new Blob([blob]));
+  //     const link = document.createElement('a');
+  //     link.href = url;
+  //     link.setAttribute('download', filename);
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     link.parentNode.removeChild(link);
+  //   })
+  //   .catch(err=> setErr(err)); 
+  // }
+
+  const downloadFile = async event => {
+    const filename =  event.target.innerText;
+    const res = await fetch (`http://localhost:5003/files/${filename}`);
+    const blob = await res.blob();
+    download (blob, filename);
   }
   
   if(submitted){
@@ -96,13 +102,13 @@ function App() {
       </div>
       <div className='w-3/5 bg-gray-200 flex justify-center'>
         <div className=''>
-        <div class="bg-red-100 mt-10 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+        {/* <div class="bg-red-100 mt-10 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
           <strong class="font-bold">{err ? err.message : null}</strong>
           <span class="block sm:inline">Something seriously bad happened.</span>
           <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
             <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
           </span>
-        </div>
+        </div> */}
           <Formik 
           initialValues = {{
             name: '', email:'',
