51b6d3705daf84d49d590005afad973a2bbbebb4
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-03-03 19:34:35 +0100

    first level auth

--- src/components/LeftPanel.js
+++ src/components/LeftPanel.js
@@ -6,7 +6,7 @@ export const LeftPanel = () => {
     <div className='w-1/2 mx-auto'>
       <div className=' p-10'>
         <div className=' my-20 max-w-full flex flex-shrink-0'>
-          <img className=' ' height={700} width src={Uploading} alt='Explainer Standing with Direction for app use' />
+          <img className=' ' height={700} width={700} src={Uploading} alt='Explainer Standing with Direction for app use' />
         </div>
         <div className='item-1 text-3xl text-center font-bold text-orange-700 '>In One Click.</div>
         <div className='text-orange-600 text-center mx-5 mb-32 font-bold text-sm'>Attach your AMF and get it parsed for you.</div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import './index.css';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
-// import {BrowserRouter} from 'react-router-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 const customHistory = createBrowserHistory();
@@ -14,7 +13,4 @@ render(
     </Router>, document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
