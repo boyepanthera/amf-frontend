b1c69eae5ae96c284041906ffed33bb4b40208b0
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-03-16 19:36:52 +0100

    rm test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const myStr = "000107      0007 CASH & TELLER SUSPENSE/CASH IN TRANSIT               4,419,276.50 Dr                       0.00                  4,419,276.50 Dr"
+
 //replace all comma with empty space
 const newStr = myStr.replace(/,/g, "");
 
@@ -6,3 +8,4 @@ const newerStr = newStr.split(/\s/).join("");
 
 //splitter
 let newestStr = newerStr.match(/[a-zA-Z]+|[0-9]+(?:\.[0-9]+)?|\.[0-9]+/g);
+console.log(newestStr);
